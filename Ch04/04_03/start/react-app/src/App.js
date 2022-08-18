f97d65eacff839300c5e9edccb247fa25ed6d27c
R S <4614260+raakasf@users.noreply.github.com>
2022-08-18 16:37:01 +0000

    useState

--- Ch04/04_03/start/react-app/src/App.js
+++ Ch04/04_03/start/react-app/src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [emotion, setEmotion ] = useState("happy");
+
   return (
     <div className="App">
-      <h1>Hello from React</h1>
+      <h1>Current emotion is {emotion}</h1>
+      <button onClick={ () => setEmotion("sad")}>Sad</button>
+      <button onClick={() => setEmotion("excited")}>Excited</button>
     </div>
   );
 }
