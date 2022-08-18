37c34909f1063338e5ba74507d0a8dd8b382335e
R S <4614260+raakasf@users.noreply.github.com>
2022-08-18 18:50:40 +0000

    use state

--- Ch05/05_02/start/react-app/src/App.js
+++ Ch05/05_02/start/react-app/src/App.js
@@ -3,12 +3,10 @@ import { useState } from "react";
 
 function App() {
   const [title, setTitle] = useState("");
-  const [color, setColor] = useState("#000000");
+  const [color, setColor] = useState("#0000000")
   const submit = (e) => {
     e.preventDefault();
     alert(`${title}, ${color}`);
-    setTitle("");
-    setColor("#000000");
   };
   return (
     <form onSubmit={submit}>
