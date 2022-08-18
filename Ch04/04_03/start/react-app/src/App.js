31fe6fba02f61d69ebc4fa6d2440a0d4e31d7875
R S <4614260+raakasf@users.noreply.github.com>
2022-08-18 16:47:50 +0000

    useeffect and usestate

--- Ch04/04_03/start/react-app/src/App.js
+++ Ch04/04_03/start/react-app/src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [emotion, setEmotion ] = useState("happy");
+  const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log(`It's ${emotion} right now`);
+  }, [emotion, secondary]);
+
+  useEffect(() => {
+    console.log(`It's ${secondary} around here!`)
+  }, [secondary]);
 
   return (
     <div className="App">
       <h1>Current emotion is {emotion}</h1>
       <button onClick={ () => setEmotion("sad")}>Sad</button>
       <button onClick={() => setEmotion("excited")}>Excited</button>
+      <h2>Current secondary emotion is {secondary}</h2>
+      <button onClick={() => setSecondary()}>Grateful</button>
     </div>
   );
 }
